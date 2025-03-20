3d3fafe1ce3b60c47c8ac21d6b9a7addcdc9b26a
OVK <aminehifdi121@gmail.com>
2025-03-20 02:44:50 +0000

    1st commit isdigit ft

---
+++ isdigit.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int isdigit(int value)
+{
+    if (value > 47 && value < 58)
+        return  value > 0;
+    return 0;
+}
+
+int main()
+{
+    char c;
+    c='0';
+    printf("Result when numeric character is passed: %d", isdigit(c));
+
+    c='+';
+    printf("\nResult when non-numeric character is passed: %d", isdigit(c));
+
+    return 0;
+}
