ad3b4864856dd40ddc6b447d5ef260b1c1dd94aa
Amine Hifdi <101675260+OVK-CyberSec@users.noreply.github.com>
2025-04-13 23:04:08 +0200

    Update libft.h

--- libft.h
+++ libft.h
@@ -4,10 +4,6 @@
 # include <string.h>
 # include <stdlib.h>
 # include <unistd.h>
-
-# include <sys/types.h>
-# include <sys/stat.h>
-# include <fcntl.h> 
 # include <stdint.h>
 
 void	*ft_memset(void *s, int c, size_t len);
